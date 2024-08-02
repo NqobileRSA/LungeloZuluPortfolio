ec898fe75998f8637109b466e5a089851e90a011
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-08-02 22:10:04 +0200

    responsive grid

--- app/pages/Archives.tsx
+++ app/pages/Archives.tsx
@@ -4,222 +4,63 @@ import React from 'react';
 type Props = {};
 
 const Archives = (props: Props) => {
+  const imageStyle =
+    'bg-cover bg-center bg-no-repeat w-full h-64 sm:h-80 md:h-96 lg:h-[400px]';
+
   return (
-    <div id="archives">
+    <div id="archives" className="container mx-auto">
       <SectionTitle title="Archives" />
-      <div className="grid">
+      <div className="grid gap-4 p-4">
         {/* 1st row */}
-        <div className="grid grid-cols-6 grid-rows-1">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2">
+          <div className={`${imageStyle} bg-[url('/radio.jpg')]`} />
           <div
-            style={{
-              background: `url("/radio.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
+            className={`${imageStyle} bg-[url('/bluegray.jpg')] sm:col-span-2`}
           />
-          <div
-            className="col-span-2"
-            style={{
-              background: `url("/bluegray.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            className="col-start-4"
-            style={{
-              background: `url("/wearhouz.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            className="col-start-5"
-            style={{
-              background: `url("/themba.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            className="col-start-6"
-            style={{
-              background: `url("/cusuin.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
+          <div className={`${imageStyle} bg-[url('/wearhouz.jpg')]`} />
+          <div className={`${imageStyle} bg-[url('/themba.jpg')]`} />
+          <div className={`${imageStyle} bg-[url('/cusuin.jpg')]`} />
         </div>
+
         {/* 2nd row */}
-        <div className="grid grid-cols-5 grid-rows-1 gap-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
+          <div className={`${imageStyle} bg-[url('/skyf.jpg')]`} />
           <div
-            style={{
-              background: `url("/skyf.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
+            className={`${imageStyle} bg-[url('/swag.jpg')] sm:col-span-2`}
           />
           <div
-            className="col-span-2"
-            style={{
-              background: `url("/swag.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            className="col-span-2 col-start-4"
-            style={{
-              background: `url("/bittercure.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
+            className={`${imageStyle} bg-[url('/bittercure.jpg')] sm:col-span-2`}
           />
         </div>
+
         {/* 3rd row */}
-        <div className="grid grid-cols-4 grid-rows-1 gap-0">
-          <div
-            style={{
-              background: `url("/combo.png")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            style={{
-              background: `url("/vibe.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            style={{
-              background: `url("/half.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            style={{
-              background: `url("/cusuin.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2">
+          <div className={`${imageStyle} bg-[url('/combo.png')]`} />
+          <div className={`${imageStyle} bg-[url('/vibe.jpg')]`} />
+          <div className={`${imageStyle} bg-[url('/half.jpg')]`} />
+          <div className={`${imageStyle} bg-[url('/cusuin.jpg')]`} />
         </div>
+
         {/* 4th row */}
-        <div className="grid grid-cols-1 grid-rows-1 gap-0">
+        <div className="grid grid-cols-1 gap-2">
           <div
-            style={{
-              background: `url("/yellow.png")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '1200px',
-            }}
+            className={`${imageStyle} bg-[url('/yellow.png')] h-[300px] sm:h-[600px] md:h-[900px] lg:h-[1200px]`}
           />
         </div>
+
         {/* 5th row */}
-        <div className="grid grid-cols-5 grid-rows-1 gap-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
           <div
-            className="col-span-2"
-            style={{
-              background: `url("/pink.png")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
+            className={`${imageStyle} bg-[url('/pink.png')] sm:col-span-2`}
           />
-          <div
-            className="col-start-3"
-            style={{
-              background: `url("/fb.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
-          <div
-            className="col-span-2 col-start-4"
-            style={{
-              background: `url("/k1.png")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '400px',
-            }}
-          />
+          <div className={`${imageStyle} bg-[url('/fb.jpg')]`} />
+          <div className={`${imageStyle} bg-[url('/k1.png')] sm:col-span-2`} />
         </div>
+
         {/* 6th row */}
-        <div className="grid grid-cols-1 grid-rows-1 gap-0">
+        <div className="grid grid-cols-1 gap-2">
           <div
-            style={{
-              background: `url("/SUNDAY.jpg")`,
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              // padding: '20px',
-              // width: '400px',
-              height: '1200px',
-            }}
+            className={`${imageStyle} bg-[url('/SUNDAY.jpg')] h-[300px] sm:h-[600px] md:h-[900px] lg:h-[95vh]`}
           />
         </div>
       </div>
